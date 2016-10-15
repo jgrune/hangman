f1bb34710ca4f524c0db326ffa2ef4f2039c8c28
Jonathan Grunewald <jonathan.grunewald1@gmail.com>
2016-10-14 23:29:20 -0400

    images updated on loss of life - code working again

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ $(document).ready(function(){
 
   var phrase = {
     array: [],
+    getWord: function(){
+      phrase.initialInput = prompt('Please enter the phrase you would like your opponent to guess!', 'Enter phrase here');
+      phrase.initialInput = phrase.initialInput.toLowerCase();
+      //initialize blank letter tiles
+      phrase.array = phrase.initialInput.split("");
+      phrase.setLetters(phrase.array);
+    },
     setLetters: function(letters){
       letters.forEach(function(letter){
         if(letter === " "){
@@ -12,51 +19,45 @@ $(document).ready(function(){
         $('#letterSpaces>section').append("<div class='" + className + "Div'><span class='letterSpan'>" + letter + "</span></div>");
       })
     },
-    getWord: function(){
-      phrase.initialInput = prompt('Please enter the phrase you would like your opponent to guess!', 'Enter phrase here');
-      phrase.initialInput = phrase.initialInput.toLowerCase();
-      //initialize blank letter tiles
-      phrase.setLetters(phrase.initialInput.split(""));
-    }
+
   }
 
-      var gameState = {
-        hangmanImage: [0,1,2,3,4,5].map(function(i){
-          return "rgba(255,255,255,0.8) url('images/Hangman-" + i + ".png') no-repeat center"
-        }),
-        unusedLetters: [],
-        score: 0,
-        timer: 0,
-        lives: 6
-      }
+  var gameState = {
+    hangmanImage: [0,1,2,3,4,5,6].map(function(i){
+      return "rgba(255,255,255,0.8) url('images/Hangman-" + i + ".png') no-repeat center"
+    }),
+    imageCounter: 1,
+    initializeScreen: function() {
+      //remove intruction text
+      $("#letterSpaces>h2").text("");
+      //hide start button
+      $("#startButton").css('visibility', 'hidden');
+      //set initial hangman image
+      $("#animation").css('background', gameState.hangmanImage[0]);
+      //set score
+      $("#score").append('<div>' + gameState.score + '</div>')
+      //initialize unused letters module
+      gameState.unusedletters = "abcdefghijklmnopqrstuvwxyz".split("");
+      gameState.unusedletters.forEach(function(value){
+        $("#usedletters").append("<span class='unused'>" + value + "</span>");
+      })
+      $('#letterSpaces').append("<h2>Guess a letter!</h2>")
+    },
+    score: 0,
+    timer: 0,
+    lives: 6
+  }
+
+
 
       //start button click event
       $("#startButton").on("click", startGame)
 
       function startGame(){
         phrase.getWord();
-
-        //remove intruction text
-        $("#letterSpaces>h2").text("");
-        //hide start button
-        $("#startButton").css('visibility', 'hidden');
-        //set initial hangman image
-        $("#animation").css('background', gameState.hangmanImage[0]);
-        //set score
-        $("#score").append('<div>' + gameState.score + '</div>')
-        //initialize unused letters module
-        gameState.unusedletters = "abcdefghijklmnopqrstuvwxyz".split("");
-        gameState.unusedletters.forEach(function(value){
-          $("#usedletters").append("<span class='unused'>" + value + "</span>");
-        })
-        $('#letterSpaces').append("<h2>Guess a letter!</h2>")
-        //begin listening for keypresses
-        $(document).on("keypress", function(){
-          // if letter in word,
-            //update letter tiles then update score
-          // else decrease lives
-          //
-        });
+        gameState.initializeScreen();
+          //begin listening for keypresses
+        $(document).on("keypress", validateLetter)
       }
 
       function validateLetter(event){
@@ -95,9 +96,8 @@ $(document).ready(function(){
       }
 
       function updateLives(){
-        var i = 0;
-        $("#animation").css('background', gameState.hangmanImage[i]);
-        i++;
+        $("#animation").css('background', gameState.hangmanImage[gameState.imageCounter]);
+        gameState.imageCounter++;
         gameState.lives--;
     }
 
