7c88c5a9eea235d7af8e0a36efd4477d729abb65
Andy <andrew.sunglae.kim@gmail.com>
2016-10-26 12:09:20 -0400

    adds comments/rubric

---
+++ rubric.md
@@ -0,0 +1,13 @@
+# Rubric
+
+## Deployment
+Performing - App is deployed to gh-pages and is congruent to what's happening in the master branch locally.
+
+## Technical Requirements
+Performing/Excelling - There are lots of great abstractions! The code is well written and semantic. Its definitely moving in the right direction towards object oriented design. Take what you've done with this code base, and see how you can apply JS classes.
+
+## Code Quality
+Excelling/Performing - Really great commenting. Your code is very clear as to what it's doing as a result. Code is well formatted and like the above comments state, well abstracted.
+
+## Creativity/Interface
+Excelling - The halloween theme was really cool! The app is just really well polished, very nice work. One minor gripe is the page refresh on resetting the game, I'm all about dat SPA (single page application) life.
--- script.js
+++ script.js
@@ -18,6 +18,7 @@ $(document).ready(function(){
                     } else {
                       var className = "letter"
                       }
+                      // slight indentation issue
         $('#letterSpaces>section').append("<div class='" + className + "Div'><span class='letterSpan'>" + letter + "</span></div>");
                   })
                 },
@@ -27,6 +28,10 @@ $(document).ready(function(){
   }
 
   var gameState = {
+    // this is a small clerical thing, but this array your mapping is a hard coded value. as another developer, i'm unsure what your trying to map
+    // constant values like this should be instantiate at the top of the scope something like this:
+    // var imageIndexes = [0,1,2,3,4,5,6] or something like this
+    // then it would be imageIndexes.map .... more code
     hangmanImage: [0,1,2,3,4,5,6].map(function(i){
       return "rgba(255,255,255,0.8) url('images/Hangman-" + i + ".png') no-repeat center"
     }),
@@ -43,6 +48,7 @@ $(document).ready(function(){
 
 
       //initialize unused letters module
+      // another constant like the comment above
       gameState.unusedletters = "abcdefghijklmnopqrstuvwxyz".split("");
       gameState.unusedletters.forEach(function(value){
         $("#usedletters").append("<span class='unused'>" + value + "</span>");
@@ -57,7 +63,10 @@ $(document).ready(function(){
 
   //set score
   $("#score").append("<div id='gameScore'>" + gameState.score + '</div>');
+  // i think you want undefined not in quotes here, or use unstrict comparison != otherwise it'll see if Storage is exactly the string "undefined"
+  // I don't think it'll ever hit the else condition if browser doesn't support session stores and may cause your program to error when it hits the code sessionStorage
   if (typeof(Storage) !== "undefined") {
+    // nice persistence!
     if(sessionStorage.highScore){
     $("#score").append('High Score:'+ '<div id="highScore">' + sessionStorage.highScore + '</div>')
     } else{
@@ -72,22 +81,29 @@ $(document).ready(function(){
   $("#resetButton").on("click", resetGame)
   $("#winButton").on("click", resetGame)
 
+  // you've already got two objects that abstract functionality, why not get this all into the gameState object? I think the start of a game has definitely somethign to do with game state
   function startGame(){
     phrase.getWord();
     gameState.initializeScreen();
+    // countDown sounds like a part of a game's state
+    // which you happen to have an object defined for that to live in.
     countDown = setInterval(startTimer, 1000);
   }
 
+  // likewise, this sounds like something that belongs in the phrase object
   function validateLetter(event){
     var letterGuess = event.key;
     var unusedIndex = gameState.unusedletters.indexOf(letterGuess);
 
     //check if letter has already been guessed
+    // this is a smell, reaeson being, is that 13, 9 , 16 will always evaluate to true and hence the if condition is really only evaluating whether the keycode is 32 or not
+    // also super minor runtime enhancement is to move this if condition above your variable instantiation. otherwise it will instantiate those variables when it doesn't need to
     if(event.keyCode !== 32 && 13 && 9 && 16){
       if($('#usedletters span').eq(unusedIndex).hasClass('used')) {
         console.log("used letter");
       } else {
         //change coloring of guessed letter
+        // how can you refactor this using toggle class? or just adding one class? its small but potentially could cut some code
         $('#usedletters span').eq(unusedIndex).removeClass('unused').addClass('used');
 
 
@@ -108,8 +124,12 @@ $(document).ready(function(){
       }
     }
 }
-
+  // it'd be cool if the score addition was variable depending on the timer
   function addToScore(){
+    // 10 here is what's known as a "magic number" better would be to create a variable at the top of the scope something like this:
+    // var scoreIncrementation = 10
+    // gameState.score += scoreIncrementation
+    // also makes the comment above the function easier to implement
     gameState.score += 10;
     $("#score #gameScore").text(gameState.score);
 
@@ -119,6 +139,7 @@ $(document).ready(function(){
     }
   }
 
+  // move this into gamestate, your already working with a couple properties in it!
   function loseLife(){
     $("#animation").css('background', gameState.hangmanImage[gameState.imageCounter]);
     gameState.imageCounter++;
@@ -128,18 +149,19 @@ $(document).ready(function(){
       }, 200);
     }
   }
-
+  // same thing
   function startTimer (){
     //begin listening for keypresses
     $(document).on("keypress", validateLetter)
 
       $("#timer>div").html(gameState.timer);
       gameState.timer--;
+      // it'd be cool if the timer got down to a certain value it would start flashing like the scores
       if(gameState.timer === -1){
         resetState('loss');
       }
     }
-
+  // same thing
   function checkWin (){
     if (phrase.correctGuesses === (phrase.array.length - phrase.spaceCount)){
       if (!sessionStorage.highScore || (gameState.score > sessionStorage.highScore)){
@@ -148,7 +170,7 @@ $(document).ready(function(){
       resetState('win');
     }
   }
-
+  // seeing a reoccuring pattern here.. this function even has the word state in it!
   function resetState(result){
     clearInterval(countDown);
     $(document).off("keypress")
@@ -165,6 +187,7 @@ $(document).ready(function(){
     }
   }
 //testing
+  // this reset function causes an entire page refresh thus ruining that single page application feel
   function resetGame(){
     window.location.reload();
   }
